063301bea1a2e40f22063b0daee6d495a987c54a
Mujeebullah Kalwar <mujeebkalwar.mk@gmail.com>
2021-04-29 19:01:58 +0500

    updated

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 #### HELPER ####
-# install these asll libs by using pip command (pip install pushbullet.py)
+# install these all libs by using pip command (pip install pushbullet.py)
 # put your push bullet access token in api_key variable
+#
+# pip3 install opencv-python
 
 import time
+import cv2 as cv
 from gpiozero import MotionSensor
 from gpiozero import LED
 from picamera import PiCamera
@@ -15,6 +18,7 @@ pir = MotionSensor(4)
 camera = PiCamera()
 camera.rotation = 180
 pb = Pushbullet(api_key)
+haar_cascade = cv.CascadeClassifier("haar_face.xml")
 
 while True:
    try:
@@ -29,11 +33,21 @@ while True:
       camera.stop_recording()
       led.off()
       camera.stop_preview()
+      capture = cv.VideoCapture('2.mp4')
+      while True:
+         istrue, frame = capture.read()
+         face_detects = haar_cascade.detectMultiScale(filename, scaleFactor=1.1, minNeighbors=8)
+         print(face_detects)
+         for (x,y,w,h) in face_detects:
+            cv.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), thickness=4)
+         if cv.waitKey(100) & 0xFF==ord("q"):
+            break
+      capture.release()
       push = pb.push_note("Motion detected", "Please see tht attached file")
       with open(filename, "rb") as file_data:
          file_data = pb.upload_file(file_data, "recording.h264")
       push = pb.push_file(**file_data)
+      cv.destroyAllWindows()
       print('Recording Sent')
    except Exception as e:
       print('ERROR:', e)
-   
